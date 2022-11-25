81d5f848bb2c06b7fba7cb84ee8de0774b215a55
franciscoldb <fluis905@gmail.com>
2022-11-25 20:00:46 -0300

    add vetor de extrato

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,42 @@
+const extrato = [
+    {
+        data: "16/06",
+        tipo: "Saida",
+        descricao: "Lojas Americanas",
+        valor: -500
+    },
+    {
+        data: "02/10",
+        tipo: "Entrada",
+        descricao: "Lojas Americanas",
+        valor: 1500
+    },
+    {
+        data: "07/10",
+        tipo: "Entrada",
+        descricao: "Salario",
+        valor: 3500
+    },
+    {
+        data: "16/10",
+        tipo: "Saida",
+        descricao: "Lojas Americanas",
+        valor: -1500
+    },
+    {
+        data: "10/11",
+        tipo: "Saida",
+        descricao: "Lojas Americanas",
+        valor: -1500
+    },
+    {
+        data: "22/11",
+        tipo: "Saida",
+        descricao: "Lojas Americanas",
+        valor: -1500
+    }
+]
+
 window.addEventListener('resize', atualizar);
 
 function atualizar() {
