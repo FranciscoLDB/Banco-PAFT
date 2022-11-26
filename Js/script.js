162bf441e3c344839ff6ad1391f02c27d64c8ecd
franciscoldb <fluis905@gmail.com>
2022-11-26 10:33:52 -0300

    tabela de extrato responsiva

--- Js/script.js
+++ Js/script.js
@@ -48,51 +48,61 @@ function mostrar_extrato(){
         let data = document.createElement('td');
         let mov = document.createElement('td');
         let desc = document.createElement('td');
-        let valor = document.createElement('td');
+        let valor = document.createElement('td');        
         
+        row.className = 'row-extrato';
         data.innerHTML = extrato[i].data;
         mov.innerHTML = extrato[i].mov;
         desc.innerHTML = extrato[i].descricao;
+        desc.className = 'desc';
         if(mov.innerHTML == "Saida"){
             valor.innerHTML = "-";
             row.style.backgroundColor = '#ff524866';
         }else{
             row.style.backgroundColor = '#77ab5966';
         }
-        valor.innerHTML += `R$${extrato[i].valor.toFixed(2) }`;
+        valor.innerHTML += `R$${extrato[i].valor.toFixed(2)}`;
 
         row.appendChild(data);
         row.appendChild(mov);
-        if (largura >= 992) {
-            row.appendChild(desc);
-        }
-        row.appendChild(valor);
-
+        row.appendChild(desc);
+        row.appendChild(valor);        
         tableBody.appendChild(row);
     }
+    tbodyMaior = tableBody.innerHTML;
+    const descricoes = document.getElementsByClassName("desc");
+    for (let i = descricoes.length; i > 0; i--) {
+        descricoes[i-1].remove();
+    }
+    tbodyMenor = tableBody.innerHTML;
+
+    atualizar();
 }
 
 window.addEventListener('resize', atualizar);
 window.addEventListener('load', atualizar);
 
+let tbodyMaior = '';
+let tbodyMenor = '';
 function atualizar() {
     var largura = window.screen.width;
+    
     if (largura >= 992) {
         tableHead.innerHTML = 
-            `<tr>
-                <th scope="col">Data</th>
-                <th scope="col">Movimentação</th>
-                <th scope="col">Descrição</th>
-                <th scope="col">Valor</th>
-            </tr>
-            `;
+        `<tr>
+        <th scope="col">Data</th>
+        <th scope="col">Movimentação</th>
+        <th scope="col">Descrição</th>
+        <th scope="col">Valor</th>
+        </tr>`;
+        tableBody.innerHTML = tbodyMaior;
     } else {
         tableHead.innerHTML = 
-            `<tr>
-                <th scope="col">Data</th>
-                <th scope="col">Movimentação</th>
-                <th scope="col">Valor</th>
-            </tr>
-            `;
+        `<tr>
+        <th scope="col">Data</th>
+        <th scope="col">Movimentação</th>
+        <th scope="col">Valor</th>
+        </tr>`;
+        tableBody.innerHTML = tbodyMenor;
     }
 };
