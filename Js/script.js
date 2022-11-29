2ff510f3c3f61bb87ba4be2058fc3b6a271a6b69
franciscoldb <fluis905@gmail.com>
2022-11-29 16:22:07 -0300

    estilo e JS tela emprestimo

--- Js/script.js
+++ Js/script.js
@@ -67,14 +67,23 @@ console.log(chavesPix);
 
 let tableHead = document.querySelector('thead');
 let tableBody = document.querySelector('tbody');
+let theadMaior = `<tr>
+    <th scope="col">Data</th>
+    <th scope="col">Movimentação</th>
+    <th scope="col">Descrição</th>
+    <th scope="col">Valor</th>
+    </tr>`;
+let theadMenor = `<tr>
+    <th scope="col">Data</th>
+    <th scope="col">Movimentação</th>
+    <th scope="col">Valor</th>
+    </tr>`;
 let tbodyMaior = '';
 let tbodyMenor = '';
 let mostrar_extrato = function (conta){
     return function (){
-        var largura = window.screen.width;
         tableBody.innerHTML = "";
         for(let i = 0; i < conta.extrato.length; i++){
-            console.log("entro no for");
             let row = document.createElement('tr');
             let data = document.createElement('td');
             let mov = document.createElement('td');
@@ -117,69 +126,112 @@ if(!(btnExtrato == null)){
     btnExtrato.addEventListener('click', mostrar_extrato(contaChico));
 }
 
-let detalhesPagamento = document.getElementById('detalhes-pagamento');
-let confirmaPagamento = document.getElementById('confirma-pagamento');
-let selectPagamento = document.getElementById('select-pagamento');
-let labelChave = document.getElementById('label-chave');
-let valor = document.querySelector('.input-valor');
-let chave = document.querySelector('.input-chave');
-
-selectPagamento.addEventListener('change', () => {
-    if(selectPagamento.value == 'boleto'){
-        labelChave.innerHTML = 'Codigo de barras';
-    } else {
-        labelChave.innerHTML = 'Chave pix';
-    }
-    reset_pagamento();
-});
-
-function reset_pagamento(){
-    detalhesPagamento.innerHTML = `
-    <div class="col-12">
-        <button type="button" class="btn btn-dark" id="btn-pagar" >Proseguir</button>
-    </div>`;
-    document.getElementById('btn-pagar').addEventListener('click', mostrar_pagamento);
-}
-chave.addEventListener('change', reset_pagamento);
-
-async function mostrar_pagamento(){    
-    let recebedor = await chavesPix[chave.value];    
+async function init_pagamento(){
+    let detalhesPagamento = document.getElementById('detalhes-pagamento');
+    let selectPagamento = document.getElementById('select-pagamento');
+    let labelChave = document.getElementById('label-chave');
+    let valor = document.querySelector('.input-valor');
+    let chave = document.querySelector('.input-chave');
     try{
-        detalhesPagamento.innerHTML =    
-            `<h2>Detalhes do pagamento</h2>
-            <p><strong>Recebedor</strong> <span>${recebedor.nome}</span></p>
-            <p><strong>Banco</strong> <span>${recebedor.banco}</span></p>
-            <p><strong>Agencia</strong> <span>${recebedor.agencia}</span></p>
-            <div class="row confirma-pagamento">
-            <div class="col-6">
-            <span>Total a pagar</span>
-            <p>R$${valor.value},00</p>
-            </div>
-            <div class="col-6">
-            <button type="button" class="btn btn-dark" id="btn-pagar" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Pagar</button>
-            </div>
-            </div>`;
-    } catch {
-        detalhesPagamento.innerHTML =    
-            `<h2>Houve um erro</h2>
-            <p><strong>Chave pix ${chave.value} nao localizada</strong></p>
-            </div>
-            <div class="col-12">
-            <button type="button" class="btn btn-dark" id="btn-pagar">Pagar</button>
-            </div>
-            </div>`;
-            document.getElementById('btn-pagar').addEventListener('click', mostrar_pagamento);
+        selectPagamento.addEventListener('change', () => {
+            if(selectPagamento.value == 'boleto'){
+                labelChave.innerHTML = 'Codigo de barras';
+            } else {
+                labelChave.innerHTML = 'Chave pix';
+            }
+            reset_pagamento();
+        });
+        chave.addEventListener('change', reset_pagamento);
+        
+        let btnPagar = document.getElementById('btn-pagar');
+        btnPagar.addEventListener('click', mostrar_pagamento);
+        console.log('Pagamento inicializado com sucesso');
+    } catch (e) {
+        console.log('Erro ao inicializar pagamento ' + e);
+    }
+
+    function reset_pagamento(){
+        detalhesPagamento.innerHTML = `
+        <div class="col-12">
+            <button type="button" class="btn btn-dark" id="btn-pagar" >Proseguir</button>
+        </div>`;
+        document.getElementById('btn-pagar').addEventListener('click', mostrar_pagamento);
+    }
+
+    async function mostrar_pagamento(){    
+        let recebedor = await chavesPix[chave.value];    
+        try{
+            detalhesPagamento.innerHTML =    
+                `<h2>Detalhes do pagamento</h2>
+                <p><strong>Recebedor</strong> <span>${recebedor.nome}</span></p>
+                <p><strong>Banco</strong> <span>${recebedor.banco}</span></p>
+                <p><strong>Agencia</strong> <span>${recebedor.agencia}</span></p>
+                <div class="row confirma-pagamento">
+                <div class="col-6">
+                <span>Total a pagar</span>
+                <p>R$${valor.value},00</p>
+                </div>
+                <div class="col-6">
+                <button type="button" class="btn btn-dark" id="btn-pagar" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Pagar</button>
+                </div>
+                </div>`;
+        } catch {
+            detalhesPagamento.innerHTML =    
+                `<h2>Houve um erro</h2>
+                <p><strong>Chave pix ${chave.value} nao localizada</strong></p>
+                </div>
+                <div class="col-12">
+                <button type="button" class="btn btn-dark" id="btn-pagar">Pagar</button>
+                </div>
+                </div>`;
+                document.getElementById('btn-pagar').addEventListener('click', mostrar_pagamento);
+        }
     }
 }
+init_pagamento();
+
+async function init_emprestimo(){
+    let valorDesejado = document.querySelector('#valor-desejado');
+    let slider = document.querySelector('.range-valor');
+    let selectEmp = document.querySelector('#numero-parcelas');
+    let btnSimularEmp = document.querySelector('#btn-simular-emprestimo');
+    let resumoParcelas = document.querySelector('#resumo-parcelas');
+    let totalAPagar = document.querySelector('#total-a-pagar');
+    let btnSolicitarEmp = document.querySelector('#btn-sol-emprestimo');
+
+    for (let i = 1; i < 12; i++) {
+        let opt = document.createElement('option');
+        opt.value = i;
+        opt.id = i;
+        opt.text = i + 'x';
+        selectEmp.appendChild(opt);
+    }
+    for (let i = 1; i < 4; i++) {
+        let opt = document.createElement('option');
+        opt.value = i * 12;
+        opt.id = i*12;
+        opt.text = (i*12) + 'x';
+        selectEmp.appendChild(opt);
+    }
+    slider.value = 2500;
     
-let pagar = function(){
-    return function(){
+    slider.addEventListener('input', () => {
+        valorDesejado.innerHTML = `R$${slider.value},00`;
+    });
 
-    }
+    btnSimularEmp.addEventListener('click', () => {
+        let numP = selectEmp.value;
+        let jurosM = (6.25**(1/12))-1;
+        let valorP = slider.value * (Math.pow((1+jurosM),numP)*jurosM)/(Math.pow((1+jurosM),numP)-1);
+        valorP = valorP.toFixed(2);
+        valorP.toString().replace('.', ',');
+        resumoParcelas.innerHTML = `${numP} de R$${valorP.toString().replace('.', ',')}`;
+        totalAPagar.innerHTML = `R$${((numP*valorP).toFixed(2)).toString().replace('.', ',')}`;
+    });
+
 }
+init_emprestimo();
 
-let btnPagar = document.getElementById('btn-pagar');
-btnPagar.addEventListener('click', mostrar_pagamento);
 
 window.addEventListener('resize', atualizar);
 window.addEventListener('load', atualizar);
@@ -188,23 +240,12 @@ function atualizar() {
     var largura = window.screen.width;    
     if (largura >= 992) {
         if(!(btnExtrato == null)){   
-            tableHead.innerHTML = 
-            `<tr>
-            <th scope="col">Data</th>
-            <th scope="col">Movimentação</th>
-            <th scope="col">Descrição</th>
-            <th scope="col">Valor</th>
-            </tr>`;
+            tableHead.innerHTML = theadMaior;
             tableBody.innerHTML = tbodyMaior;
         }
     } else {
         if(!(btnExtrato == null)){
-            tableHead.innerHTML = 
-            `<tr>
-            <th scope="col">Data</th>
-            <th scope="col">Movimentação</th>
-            <th scope="col">Valor</th>
-            </tr>`;
+            tableHead.innerHTML = theadMenor;
             tableBody.innerHTML = tbodyMenor;
         }
     }
